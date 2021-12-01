17f5bfb556d24362527174f9db3747df0cb8d2b9
alberto012 <rechavarria2@gmail.com>
2021-12-01 18:38:45 -0300

    all done

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,7 +10,15 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+objeto({
+rojo:7,
+verde:3,
+azul: 5
+})
+var resutado=  Object.keys(objeto).map(function(matriz){
+  return number(matriz), objet[matriz]
 }
+
 
 
 function numberOfCharacters(string) {
@@ -18,8 +26,7 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-}
-
+let 
 
 function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
